e4125951cb680a0db854fdded2f1b0dfee6ced97
Nate Mills <natemills018@gmail.com>
2023-09-30 22:23:07 -0400

    Trying to add functionality so that the button can loop through the array incrementally

--- DOM-Manipulation/scripts.js
+++ DOM-Manipulation/scripts.js
@@ -53,7 +53,9 @@ document.addEventListener("DOMContentLoaded", function () {
     spanButton.addEventListener("click", function () {
         let textDiv = document.getElementById('emptyDiv');
         let spanElement = document.createElement('span');
-        let spanText = document.createTextNode('Thy name is Nathaniel');
+        let spanText = document.createTextNode('This button creates text within a span element');
+        spanText.className = 'spanText';
+
 
         spanElement.appendChild(spanText);
         textDiv.appendChild(spanElement);
@@ -62,25 +64,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
-    let listedButton = document.getElementById('listedButton');
+
     let friendCount
 
-    function addFriend() {
-        let list = document.getElementById('friends');
+    let friends = ['Nate', 'Jordan', 'Noelle', 'Nick', 'Courtney', 'Silas', 'Roux', 'Hargrave', 'Knuckles']
+
+
+
+    let listedButton = document.getElementById('listedButton');
+
+    listedButton.addEventListener("click", function () {
        
-        for (let i = 0; i < friends.length; i++) {
+            let list = document.getElementById('friends');
             let names = document.createElement('li');
-            let friendNames = document.createTextNode(friends[i]);
+            let theirNames = friends[i];
+            let friendNames = document.createTextNode(theirNames++);
             names.appendChild(friendNames);
             list.appendChild(names);
+           
+
+    
 
 
-        }
-    }
 
-    let friends = ['Nate', 'Jordan', 'Noelle', 'Nick', 'Courtney', 'Silas', 'Roux', 'Hargrave', 'Knuckles'];
+    })
 
-    listedButton.addEventListener("click", addFriend);
+
 
 
 })
